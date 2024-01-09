1c2e77f5762d6d2666ddde174f1c73c01d052cbf
sixpoint9 <133214364+sixpoint9@users.noreply.github.com>
2024-01-09 17:58:26 +0100

    Configure MongoDB in Program.cs

--- Program.cs
+++ Program.cs
@@ -1,18 +1,30 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddControllers();
-builder.Services.AddMongoDB(options => options.UseNewUrlParser());
-builder.Services.Configure<MvcCoreOptions>(options => options.EnableSsl = true);
+
+// Configure MongoDB connection
+var connectionString = builder.Configuration.GetConnectionString("MongoDB");
+builder.Services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+builder.Services.AddScoped<GameRepository>(); // Assuming you have a GameRepository class
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
-   app.UseExceptionHandler("/Error");
-   // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-   app.UseHsts();
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios.
+    app.UseHsts();
 }
 
 app.UseHttpsRedirection();
@@ -23,5 +35,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
+app.MapControllers();
 
 app.Run();
